3d9511727cf3ce81aa67211b8fa0d0bfda32cec8
jjjjonathan <hornathanjon@gmail.com>
2021-04-08 22:27:58 -0500

    add login talk to api/login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,30 @@ import React, { useState, useEffect } from 'react';
 import Blog from './components/Blog';
 import Login from './components/Login';
 import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
 
     console.log('Logging in with', username, password);
+
+    try {
+      const newUser = await loginService.login({
+        username,
+        password,
+      });
+      setUser(newUser);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
