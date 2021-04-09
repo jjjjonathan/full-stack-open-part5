06ec6e23c0e6aa34a457487f1fdf75e40e5d4d02
jjjjonathan <hornathanjon@gmail.com>
2021-04-08 22:11:07 -0500

    add Login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import Login from './components/Login';
+import blogService from './services/blogs';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    console.log('Logging in with', username, password);
+  };
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
   return (
     <div>
+      <Login
+        onLogin={handleLogin}
+        username={username}
+        onUsernameChange={(event) => {
+          setUsername(event.target.value);
+        }}
+        password={password}
+        onPasswordChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
