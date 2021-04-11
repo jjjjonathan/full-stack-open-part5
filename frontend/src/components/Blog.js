0fff05e94e87f60ab5b73bf711875a473af5c815
jjjjonathan <hornathanjon@gmail.com>
2021-04-11 18:43:45 -0500

    exercise 5.7 complete

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} --- by {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? 'none' : '' };
+  const showWhenVisible = { display: visible ? '' : 'none' };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div style={{ border: '1px solid black', margin: 10, padding: 10 }}>
+      <strong>{blog.title}</strong> by {blog.author}
+      <button style={hideWhenVisible} onClick={toggleVisibility}>
+        Show details
+      </button>
+      <div style={showWhenVisible}>
+        <ul>
+          <li>{blog.url}</li>
+          <li>
+            Likes: {blog.likes}
+            <button>Like</button>
+          </li>
+        </ul>
+        <button onClick={toggleVisibility}>Hide details</button>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
