ac79d9c2aef0ccd13b0760f1e8a874c774d8cfdd
jjjjonathan <hornathanjon@gmail.com>
2021-04-11 22:13:26 -0500

    exercise 5.9 complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -130,9 +130,11 @@ const App = () => {
           }}
         />
       </Togglable>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
     </div>
   );
 
